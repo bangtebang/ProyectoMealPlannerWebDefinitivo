e015dce3835fef9e2acc0749da3b93bf81cd3b4a
bangtebang <e.diaz09@uformail.cl>
2023-11-30 16:31:01 -0300

    subo validacion de datos

--- src/main/java/controller/RegistroUsuarioServlet.java
+++ src/main/java/controller/RegistroUsuarioServlet.java
@@ -28,28 +28,58 @@ public class RegistroUsuarioServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher respuesta = req.getRequestDispatcher("/registroErroneo.jsp");
-        if (req.getParameter("edad").length() != 0 && req.getParameter("nombre").length() != 0 &&
-                req.getParameter("rut").length() != 0 && req.getParameter("sexo").length() != 0 && req.getParameter("peso").length() != 0 && req.getParameter("estatura").length() != 0 && req.getParameter("email").length() != 0 && req.getParameter("telefono").length() != 0 && req.getParameter("direccion").length() != 0) {
-            String nombre = req.getParameter("nombre");
-            int edad = Integer.parseInt(req.getParameter("edad"));
-            String rut = req.getParameter("rut");
-            String sexo = req.getParameter("sexo");
-            double peso = Double.parseDouble(req.getParameter("peso"));
-            double estatura = Double.parseDouble(req.getParameter("estatura"));
-            String email = req.getParameter("email");
-            String telefono = req.getParameter("telefono");
-            String direccion = req.getParameter("direccion");
-            Usuario usuario = new Usuario(nombre, edad, rut, sexo, peso, estatura, email, telefono, direccion);
-            try {
+
+        try {
+            if (validarParametros(req) && validarDatos(req)) {
+                String nombre = req.getParameter("nombre");
+                int edad = Integer.parseInt(req.getParameter("edad"));
+                String rut = req.getParameter("rut");
+                String sexo = req.getParameter("sexo");
+                double peso = Double.parseDouble(req.getParameter("peso"));
+                double estatura = Double.parseDouble(req.getParameter("estatura"));
+                String email = req.getParameter("email");
+                String telefono = req.getParameter("telefono");
+                String direccion = req.getParameter("direccion");
+
+                Usuario usuario = new Usuario(nombre, edad, rut, sexo, peso, estatura, email, telefono, direccion);
+
                 if (agregarUsuario(usuario)) {
                     req.setAttribute("usuario", usuario);
                     respuesta = req.getRequestDispatcher("registroValido.jsp");
                 }
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
             }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+
         respuesta.forward(req, resp);
+    }
+
+    private boolean validarParametros(HttpServletRequest req) {
+        return req.getParameter("edad") != null && req.getParameter("nombre") != null &&
+                req.getParameter("rut") != null && req.getParameter("sexo") != null &&
+                req.getParameter("peso") != null && req.getParameter("estatura") != null &&
+                req.getParameter("email") != null && req.getParameter("telefono") != null &&
+                req.getParameter("direccion") != null;
+    }
+    private boolean validarDatos(HttpServletRequest req) {
+        try {
+            int edad = Integer.parseInt(req.getParameter("edad"));
+            double peso = Double.parseDouble(req.getParameter("peso"));
+            double estatura = Double.parseDouble(req.getParameter("estatura"));
+
+            if (edad <= 0 || peso <= 0 || estatura <= 0) {
+                return false;
+            }
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     private boolean agregarUsuario(Usuario usuario) throws ClassNotFoundException {
